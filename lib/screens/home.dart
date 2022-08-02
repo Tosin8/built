49bd6996b5665e9a931a62f0dc4ec8566322dd4b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-08-02 20:55:52 +0100

    Update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,45 +6,62 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset('assets/images/logo.png'),
-              const Spacer(),
-              const Icon(Icons.notifications),
-              const Icon(Icons.switch_account),
-            ],
-          ),
-          const Text('Good Morning',
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                color: Colors.black12,
-              )),
-          const Text('Choose your \n workout today',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25)),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Image.asset('assets/images/logo.png'),
+                const Spacer(),
+                const Icon(Icons.notifications),
+                const Icon(Icons.power_off),
+              ],
             ),
-            width: 120,
-            height: 50,
-            child: const TextField(
-                decoration: InputDecoration(
-                    hintText: 'Search',
-                    icon: Icon(
-                      Icons.search,
-                    ))),
-          ),
-          const Text('Recently Workouts', style: TextStyle(color: Colors.black))
-        ],
-      )),
+            const Text('Good Morning',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black12,
+                )),
+            const Text('Choose your \n workout today',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25)),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              width: 120,
+              height: 50,
+              child: const TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Search',
+                      icon: Icon(
+                        Icons.search,
+                      ))),
+            ),
+            const Text('Recently Workouts',
+                style: TextStyle(color: Colors.black)),
+            ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  width: 40,
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/6.jpg'),
+                        fit: BoxFit.fill),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
