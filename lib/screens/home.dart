986ea667b7443fd31e2fb580907615a511df18ab
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-08-02 13:45:29 +0100

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,19 +5,26 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Column(
       children: [
         Row(
           children: [
-            Image.asset(
-              'assets/images/logo.png',
-            ),
-            Spacer(),
-            Icon(Icons.notifications),
-            Icon(Icons.poweroff),
+            Image.asset('assets/images/logo.png'),
+            const Spacer(),
+            const Icon(Icons.notifications),
+            const Icon(Icons.switch_account),
           ],
-        )
+        ),
+        const Text('Good Morning',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Colors.black12,
+            )),
+        const Text('Choose your /n workout today',
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25))
       ],
     ));
   }
